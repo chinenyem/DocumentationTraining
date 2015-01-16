e35de177a30c00e830f3d79adf9edffe4cff381f
Chinenyem Nwadiugwu <chnw1947@gmail.com>
2015-01-16 11:29:30 -0500

    gdgPractice
    adding sum of fields, and setting another field to the total of the
    fields

---
+++ excercise/sumfields.js
@@ -0,0 +1,20 @@
+
+
+$(document).ready(function(){
+
+
+$("input[name = 'c_chin2'], input[name = 'c_chin1']").keyup(function(){
+    
+    var sum = 0;
+
+    
+    $("input[name = 'c_chin2'], input[name = 'c_chin1']").each(function(){
+        sum += Number($(this).val());
+    });
+
+    $("input[name = 'c_chintotal123']").val(sum);
+
+
+});
+
+});
